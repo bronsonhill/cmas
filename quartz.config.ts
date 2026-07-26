fc1d25f950fc71aa1b1ed02a8aff1a7866c6c6b2
Bronson Hill <bronson.hill@yahoo.com.au>
2026-07-26 17:38:46 +1000

    rename wiki

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "CMAS Wiki",
+    pageTitle: "Computational Modelling and Simulation Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
